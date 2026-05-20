ec62a8b109d8d21f57132000169af869a540e231
Tom Six <tom@webatvantage.be>
2026-05-20 09:13:41 +0200

    Set up GitHub Actions workflow for publishing to JetBrains Marketplace and configure signing/publishing in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,16 @@ intellijPlatform {
             }
         }
     }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
 }
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
